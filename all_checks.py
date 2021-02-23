44678793afa06b7103f32effd8f3369106e5d3ef
davidjtaxer <davidjtaxer@gmail.com>
2021-02-23 18:07:54 -0500

    Call check_reboot from main, exit with one on error

--- all_checks.py
+++ all_checks.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 import os
+import sys
+
 def check_reboot():
     """Returns True if the computer has a pending reboot"""
     return os.path.exist("/run/reboot-required")
+
 def main():
-    pass
+    if check_reboot():
+        print("Pending reboot.")
+        sys.exit(1)
 
 main()
