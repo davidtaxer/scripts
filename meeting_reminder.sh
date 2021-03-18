0f9301ea3c20547de7e0442563ec951fb2cbaa35
David Taxer <davidjtaxer@gmail.com>
2021-03-18 18:08:57 -0400

    added new files meeting_reminder.sh and send_reminder.py

---
+++ meeting_reminder.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+meeting_info=$(zenity --forms \
+    --title 'Meeting' --text 'Reminder information' \
+    --add-calendar 'Date' --add-entry 'Title' \
+    --add-entry 'Emails' \
+    2>/dev/null)
+
+echo $meeting_info
+
+if [[ -n "$meeting_info" ]]; then
+    python3 send_reminders.py "$meeting_info"
+fi
