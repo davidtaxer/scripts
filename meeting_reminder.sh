46b4a9a9cd710a4ff7c6c35ed6df0644cfe0923e
David Taxer <davidjtaxer@gmail.com>
2021-03-18 18:52:35 -0400

    change the shell script to use the -- forms-date-format parameter and set the format to %y -%m-%d. See line #7 of script

--- meeting_reminder.sh
+++ meeting_reminder.sh
@@ -4,6 +4,7 @@ meeting_info=$(zenity --forms \
     --title 'Meeting' --text 'Reminder information' \
     --add-calendar 'Date' --add-entry 'Title' \
     --add-entry 'Emails' \
+    --forms-date-format='%Y-%m-%d' \
     2>/dev/null)
 
 echo $meeting_info
