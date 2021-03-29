e88b2d8fd4caa6204ba8fc76da0514d22c934292
David Taxer <davidjtaxer@gmail.com>
2021-03-29 14:40:56 -0400

    added task4

--- multisync.py
+++ multisync.py
@@ -4,7 +4,7 @@ def run(task):
   # Do something with task here
     print("Handling {}".format(task))
 if __name__ == "__main__":
-  tasks = ['task1', 'task2', 'task3']
+  tasks = ['task1', 'task2', 'task3', 'task4']
   # Create a pool of specific number of CPUs
   p = Pool(len(tasks))
   # Start each task within the pool
