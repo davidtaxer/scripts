0baed3c0edeeebd8edf884a93b5a5dc4818f14b3
David Taxer <davidjtaxer@gmail.com>
2021-04-01 18:23:15 -0400

    working version of dailysync.py

--- dailysync.py
+++ dailysync.py
@@ -1,23 +1,36 @@
 #!/usr/bin/env python3
 
+import multiprocessing
 import subprocess, os
 from multiprocessing import Pool
 import re
+import time
 
-src = "/media/david/SSD-FILES/scripts/checks"
+start = time.perf_counter()
+
+src = "/media/david/SSD-FILES/Google Autmation with Python"
+dest = "/media/david/BACKUP-TEST"
 
 
-def run(directory):
-    src = directory
-    dest = "/media/david/BACKUP-TEST"
-    print("Moving Data from {} to {}".format(src,dest))
-    subprocess.call(["rsync", "-arq", src, dest])
+#def main(directory):
+print("Moving Data from {} to {}".format(src, dest))
 
 
 if __name__ == "__main__":
-  dir_list = []
-  for root, dirs, files in os.walk(src, topdown=False):
-      for name in dirs:
-          dir_list.append(os.path.join(root, name))
-  p = Pool(len(dir_list))
-  p.map(run, dir_list)
+
+    directories = []
+
+    for root, dirs, files in os.walk(src):
+        for d in dirs:
+            print(d)
+            directories.append(d)
+        break
+    len_d = len(directories)
+    print(str(len_d) + ' directories  found')
+
+    pool = Pool(multiprocessing.cpu_count())
+    pool.apply(subprocess.call, args=(["rsync", "-arq", src, dest],))
+
+finish = time.perf_counter()
+
+print(f'Finished in {round(finish-start, 2)} second(s)')
