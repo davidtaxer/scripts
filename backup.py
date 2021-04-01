4a5636e753804addbd79df072c846c2285f9be0e
David Taxer <davidjtaxer@gmail.com>
2021-04-01 16:36:45 -0400

    modifications to get script working and removed sections that were not needed

--- backup.py
+++ backup.py
@@ -10,11 +10,11 @@ from sh import rsync
 
 def check_dir_exist(os_dir):
     if not os.path.exists(os_dir):
-        print os_dir, "does not exist."
+        print(os_dir, "does not exist.")
         exit(1)
 
 def confirm():
-    gogo = raw_input("Continue? yes/no\n")
+    gogo = input("Continue? yes/no\n")
     global exit_condition
     if gogo == 'yes':
         exit_condition = 0
@@ -23,45 +23,29 @@ def confirm():
         exit_condition = 1
         return exit_condition
     else:
-        print "Please answer with yes or no."
+        print("Please answer with yes or no.")
         confirm()
 
-def delete_files(ending):
-    for r, d, f in os.walk(backup_path):
-        for files in f:
-            if files.endswith("." + ending):
-                os.remove(os.path.join(r, files))
 
 # Specify what and where to backup.
-backup_path = raw_input("What should be backed up today?\n")
+backup_path = input("What should be backed up today?\n")
+#backup_path = "/media/david/SSD-FILES/scripts/checks"
 check_dir_exist(backup_path)
-print "Okay", backup_path, "will be saved."
+print("Okay", backup_path, "will be saved.")
 time.sleep(3)
 
-backup_to_path = raw_input("Where to backup?\n")
+backup_to_path = input("Where to backup?\n")
+#backup_to_path = "/media/david/BACKUP-TEST"
 check_dir_exist(backup_to_path)
+print("Okay", backup_to_path, "exists.")
 
-# Delete files first
-print "First, let's cleanup unnecessary files in the backup path."
-file_types = ["tmp", "bak", "dmp"]
-for file_type in file_types:
-    print "Delete", file_type, "files?"
-    confirm()
-    if exit_condition == 0:
-        delete_files(file_type)
 
-# Empty trash can
-print "Empty trash can?"
-confirm()
-if exit_condition == 0:
-    print "Emptying!"
-    shutil.rmtree(os.path.expanduser("~/.local/share/Trash/files"))
 
 # Do the actual backup
-print "Doing the backup now!"
+print("Doing the backup now!")
 confirm()
 if exit_condition == 1:
-        print "Aborting!"
+        print("Aborting!")
         exit(1)
 
 rsync("-auhv", "--delete", "--exclude=lost+found", "--exclude=/sys", "--exclude=/tmp", "--exclude=/proc",
