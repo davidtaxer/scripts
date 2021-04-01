09ca05ffe6415ec16c76f7861b1ae41503f91e5e
David Taxer <davidjtaxer@gmail.com>
2021-03-31 21:27:08 -0400

    commented out all lined of sync.py script due to defective script causing all files duplicate in theie own directory

--- sync.py
+++ sync.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
-
-import subprocess, os
-from multiprocessing import Pool
-import re
-
-src = "/media/david/SSD-FILES"
-
-
-def run():
-    dest = re.sub("(/media/david/USB DISK2)", r"\1"+"_backup",src)
-    print("Moving Data from {} to {}".format(src,dest))
-    subprocess.call(["rsync", "-zavh", src, dest])
-
-
-if __name__ == "__main__":
-  dir_list = []
-  for root, dirs, files in os.walk(src, topdown=False):
-      for name in dirs:
-          dir_list.append(os.path.join(root, name))
-  p = Pool(len(dir_list))
-  p.map(run, dir_list)
+#
+#import subprocess, os
+#from multiprocessing import Pool
+#import re
+#
+#src = "/media/david/SSD-FILES"
+#
+#
+#def run():
+#    dest = re.sub("(/media/david/USB DISK2)", r"\1"+"_backup",src)
+#    print("Moving Data from {} to {}".format(src,dest))
+#    subprocess.call(["rsync", "-zavh", src, dest])
+#
+#
+#if __name__ == "__main__":
+#  dir_list = []
+#  for root, dirs, files in os.walk(src, topdown=False):
+#      for name in dirs:
+#          dir_list.append(os.path.join(root, name))
+#  p = Pool(len(dir_list))
+#  p.map(run, dir_list)
