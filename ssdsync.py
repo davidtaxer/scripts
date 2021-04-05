5107ce52378048726139d3ce8d359623f79eeb8d
David Taxer <davidjtaxer@gmail.com>
2021-04-05 15:45:31 -0400

    fixed string typo added --delete whan calling rsync

--- ssdsync.py
+++ ssdsync.py
@@ -8,19 +8,18 @@ import re
 import time
 import shutil
 
-start = time.perf_counter()
 
 def confirm():
-    gogo = input("Continue? yes/no\n")
+    gogo = input("Continue? y/n\n")
     global exit_condition
-    if gogo == 'yes':
+    if gogo == 'y':
         exit_condition = 0
         return exit_condition
-    elif gogo == "no":
+    elif gogo == "n":
         exit_condition = 1
         return exit_condition
     else:
-        print("Please answer with yes or no.")
+        print("Please answer with y or n.")
         confirm()
 
 confirm()
@@ -28,11 +27,15 @@ if exit_condition == 1:
         print("Aborting!")
         exit(1)
 
+start = time.perf_counter()
+
 src = "/media/david/SSD-FILES"
-dest = "/media/david/BACKUP-TEST"
+# dest = "/media/david/BACKUP-TEST"
+dest = "/media/david/USB STICK"
 
 
 #def main(directory):
+print('')
 print("Syncing Data from (source){} to (destination){}".format(src, dest))
 print('')
 
@@ -41,18 +44,18 @@ if __name__ == "__main__":
     directories = []
 
     for root, dirs, files in os.walk(src):
-        for d in dirs:
-            print(d)
-            directories.append(d)
+        for directory in dirs:
+            print(directory)
+            directories.append(directory)
 
         break
     len_d = len(directories)
     print('')
-    print('Backed up '+ str(len_d) + ' directories')
+    print('Backing up '+ str(len_d) + ' directories')
     print('')
 
     pool = Pool(multiprocessing.cpu_count())
-    pool.apply(subprocess.call, args=(["rsync", "-arq", src, dest],))
+    pool.apply(subprocess.call, args=(["rsync", "-arq", src, dest, "--delete"],))
 
 finish = time.perf_counter()
 
