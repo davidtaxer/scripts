8a9a75bdbfaf157976952279acf702a4dffecc41
David Taxer <davidjtaxer@gmail.com>
2021-04-08 17:32:01 -0400

    added comments in script

--- ssdsync.py
+++ ssdsync.py
@@ -10,6 +10,7 @@ import shutil
 
 
 def confirm():
+    """asks for confirmation if the user wants to run the program"""
     gogo = input("Continue? y/n\n")
     global exit_condition
     if gogo == 'y':
@@ -23,25 +24,27 @@ def confirm():
         confirm()
 
 confirm()
+# exit with the message "Aborting" printed to the screen.
 if exit_condition == 1:
         print("Aborting!")
         exit(1)
 
+# start the time couner for backup time calculation
 start = time.perf_counter()
 
+# source and destination
 src = "/media/david/SSD-FILES"
 dest = "/media/david/USB STICK"
 
-
-#def main(directory):
+# message for user stating syncing fron sourece to destination
 print('')
 print("Syncing Data from (source){} to (destination){}".format(src, dest))
 print('')
 
 if __name__ == "__main__":
-
+    # create list of directories
     directories = []
-
+    # traverse the directories
     for root, dirs, files in os.walk(src):
         for directory in dirs:
             print(directory)
@@ -49,6 +52,7 @@ if __name__ == "__main__":
 
         break
     len_d = len(directories)
+    # message for user of how many directories are being backed up
     print('')
     print('Backing up '+ str(len_d) + ' directories')
     print('')
@@ -56,6 +60,8 @@ if __name__ == "__main__":
     pool = Pool(multiprocessing.cpu_count())
     pool.apply(subprocess.call, args=(["rsync", "-arq", src, dest, "--delete"],))
 
+# finish time counter for backup time message
 finish = time.perf_counter()
 
+# message for user telling how long the backup took
 print(f'Finished in {round(finish-start, 2)} second(s)')
