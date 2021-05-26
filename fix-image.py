006ada30a7388f99a223d85a7b56423324e19a9b
David Taxer <davidjtaxer@gmail.com>
2021-05-25 23:08:05 -0400

    added jpeg parameter in im.save, cleaned up scrip by combinig convert, resize, and rotate in line 21.  line 22 is now correct for im.save.  commented try except blocks. script now operating correctly.

--- fix-image.py
+++ fix-image.py
@@ -6,19 +6,22 @@ from PIL import Image
 cwd = os.getcwd()
 path = cwd + '/images/'
 new_path = '/opt/icons/'
-location = "/media/david/SSD-FILES/scripts/images"
-size = (128, 128)
+
+#location = '/media/david/SSD-FILES/scripts/images'
 
 if not os.path.exists(new_path):
     os.makedirs(new_path)
 
 for files in os.listdir(path):
-    if not files.startswith('.'):
-        im = Image.open(path + files)
-        im.convert('RGB')
-        im.resize(size)
-        im.rotate(-90)
-        im.save(new_path + files, '.jpg')
-        im.close()
+    #try:
+        if not files.startswith('.'):
+            with Image.open(path + files) as im:
 
-#print(os.listdir(location))
+        #im = Image.open(path + files)
+                im=im.convert('RGB').resize((128, 128)).rotate(-90)
+                im=im.save(new_path + files, 'jpeg', quality=100)
+                #im.close()
+#    except Exception as e:
+#        print("{} for {}".format(e, files))
+
+#print(os.listdir(path))
