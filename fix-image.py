e1f4535010ffeb98df130a88741eddbffa6350a7
David Taxer <davidjtaxer@gmail.com>
2021-05-25 21:38:30 -0400

    added variables for paths and fixed iteration with listdir, script not fully functional

--- fix-image.py
+++ fix-image.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
-import os, sys
+import os
+import sys
 from PIL import Image
 
-
+cwd = os.getcwd()
+path = cwd + '/images/'
+new_path = '/opt/icons/'
 location = "/media/david/SSD-FILES/scripts/images"
 size = (128, 128)
 
-for file in location:
-    newfile = os.path.splitext(file)[0] + ".jpg"
-    if file != newfile:
-        with Image.open(file) as im:
-            im.resize((128, 128)).rotate(90).save(newfile, "JPEG")
+if not os.path.exists(new_path):
+    os.makedirs(new_path)
+
+for files in os.listdir(path):
+    if not files.startswith('.'):
+        im = Image.open(path + files)
+        im.convert('RGB')
+        im.resize(size)
+        im.rotate(-90)
+        im.save(new_path + files, '.jpg')
+        im.close()
+
+#print(os.listdir(location))
