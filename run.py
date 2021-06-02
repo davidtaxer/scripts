e2e62ed7f8cb254cedcf379ad3310b3a0c20e381
David Taxer <davidjtaxer@gmail.com>
2021-06-02 17:50:59 -0400

    created variables for current working path, .txt file path, and created a list of .txt files in path.  script currently ptints items list

--- run.py
+++ run.py
@@ -2,3 +2,16 @@
 
 import os
 import requests
+
+#get current working directory
+cwd = os.getcwd()
+
+#path to .txt files for this script
+path = cwd + "/data/feedback"
+
+#create a list of items in path
+items = [f for f in os.listdir(path) if os.path.isfile( os.path.join(path, f) )]
+
+#itterate through files in the directory
+#for files in os.listdir(path):
+print(items)
