d0c936bfb8aea6551a8b5f8d6172da1511567483
David Taxer <davidjtaxer@gmail.com>
2021-06-03 18:08:05 -0400

    added for loop to iterate over files in items and create a dictoinary with the info.  only the first file is printing to the screen currently

--- run.py
+++ run.py
@@ -7,11 +7,22 @@ import requests
 cwd = os.getcwd()
 
 #path to .txt files for this script
-path = cwd + "/data/feedback"
+path = cwd + "/data/feedback/"
 
-#create a list of items in path
-items = [f for f in os.listdir(path) if os.path.isfile( os.path.join(path, f) )]
+# create a list of items in path
+#items = [f for f in os.listdir(path) if os.path.isfile( os.path.join(path, f) )]
+items = os.listdir(path)
+
+# (uncomment print line to check if items variable creats a list
+#  of files in path correctly)
+
+print(items)
 
 #itterate through files in the directory
-#for files in os.listdir(path):
-print(items)
+for file in items:
+    with open (path + file) as info:
+        data = info.read().split('\n')
+        dict = {'title':data[0], 'name':data[1], 'date':data[2], 'feedback':data[3]}
+
+
+print(dict)
