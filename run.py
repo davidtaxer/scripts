4285bd959f108982e003ebbeddab6bdbe9470dde
David Taxer <davidjtaxer@gmail.com>
2021-06-03 23:13:41 -0400

    removed list of dictionaries and aded requests.post() line27 added print status_code  line28

--- run.py
+++ run.py
@@ -7,23 +7,25 @@ import requests
 cwd = os.getcwd()
 
 #path to .txt files for this script
-path = cwd + "/data/feedback/"
+path ="/data/feedback/"
 
 # create a list of items in path
-#items = [f for f in os.listdir(path) if os.path.isfile( os.path.join(path, f) )]
 items = os.listdir(path)
 
-# (uncomment print line to check if items variable creats a list
-#  of files in path correctly)
+# uncomment print line to check if items variable
+# creats a list of files in path correctly
 
 #print(items)
 
-#itterate through files in the directory and save them as a list of dictionaries
-list_of_files = []
-for file in items:
-    with open (path + file) as info:
-        data = info.read().split('\n')
-        file_dict = {'title':data[0], 'name':data[1], 'date':data[2], 'feedback':data[3]}
-        list_of_files.append(file_dict)
+#itterate through files in the directory and store as a dictionary
 
-print(list_of_files)
+
+for file in items:
+        with open (path + file) as info:
+                data = info.read().split('\n')
+                file_dict = {'title':data[0], 'name':data[1], 'date':data[2], "feedback":data[3]}
+                response = requests.post("http://35.202.95.65/feedback/", json=file_dict)
+                print(response.status_code)
+
+
+#print(list_of_files)
