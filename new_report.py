2916a1512c9b994a92ff1e6c21e4dffe848c7da9
David Taxer <davidjtaxer@gmail.com>
2021-06-10 17:44:56 -0400

    centered the text that prints on the pdf and changed the positon of the image

--- new_report.py
+++ new_report.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
-
-
 from fpdf import FPDF
+from PIL import Image, ImageFilter
+import time
+import concurrent.futures
 
-name ='David'
+width = 210
+height = 297
+size = (1200, 1200)
+
 pdf = FPDF()
 pdf.add_page()
-pdf.set_font('Arial', 'B', 16)
-pdf.cell(40, 10, f'Hello World, my name is {name}!')
+pdf.set_font('Arial', 'BU', 16)
+pdf.cell(0, 0, 'Hello World, my name is David!', 0, 0, 'C')
+
+img = ('/media/david/SSD-FILES/scripts/photo-1549692520-acc6669e2f0c.jpg')
+
+pdf.image(img, 50, 30, width-100, height-100)
+
 pdf.output('tuto1.pdf', 'F')
