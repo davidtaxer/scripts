f6da197c95e9823744045d7be597415e72cc38c2
David Taxer <davidjtaxer@gmail.com>
2021-06-10 16:35:10 -0400

    added f string format on line 10 of script and modifies string that gets printed to pdf

--- new_report.py
+++ new_report.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python3
+
+
 from fpdf import FPDF
 
+name ='David'
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font('Arial', 'B', 16)
-pdf.cell(40, 10, 'Hello World!')
+pdf.cell(40, 10, f'Hello World, my name is {name}!')
 pdf.output('tuto1.pdf', 'F')
