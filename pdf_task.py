344e255d6ef0f4447e7db6b5acebb092d6b915b1
David Taxer <davidjtaxer@gmail.com>
2021-06-11 18:57:23 -0400

    pdf_task.py final version working script per lesson plan

--- pdf_task.py
+++ pdf_task.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Paragraph, Spacer, Table, Image
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib import colors
+from reportlab.graphics.shapes import Drawing
+from reportlab.graphics.charts.piecharts import Pie
+
 fruit = {
   "elderberries": 1,
   "figs": 1,
@@ -14,21 +21,27 @@ table_data = []
 for k, v in fruit.items():
     table_data.append([k, v])
 
-#print(table_data)
+print(table_data)
 
-from reportlab.platypus import SimpleDocTemplate
 report = SimpleDocTemplate("/media/david/SSD-FILES/scripts/report.pdf")
-
-from reportlab.platypus import Paragraph, Spacer, Table, Image
-from reportlab.lib.styles import getSampleStyleSheet
 styles = getSampleStyleSheet()
-
 report_title = Paragraph("A Complete Inventory of My Fruit", styles["h1"])
-
 report_table = Table(data=table_data)
-
-from reportlab.lib import colors
 table_style = [('GRID', (0,0), (-1,-1), 1, colors.black)]
 report_table = Table(data=table_data, style=table_style, hAlign="LEFT")
 
-report.build([report_title, report_table])
+report_pie = Pie(width=600, height=600)
+
+report_pie.data = []
+report_pie.labels = []
+for fruit_name in sorted(fruit):
+    report_pie.data.append(fruit[fruit_name])
+    report_pie.labels.append(fruit_name)
+
+print(report_pie.data)
+print(report_pie.labels)
+
+report_chart = Drawing()
+report_chart.add(report_pie)
+
+report.build([report_title, report_table, report_chart])
